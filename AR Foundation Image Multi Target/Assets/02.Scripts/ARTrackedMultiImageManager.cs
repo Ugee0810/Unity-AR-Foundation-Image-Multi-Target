3db9eff388e7f9599e95eaff1c1e07a6de123237
mskim <85896566+Ugee0810@users.noreply.github.com>
2022-11-21 18:53:37 +0900

    .

--- AR Foundation Image Multi Target/Assets/02.Scripts/ARTrackedMultiImageManager.cs
+++ AR Foundation Image Multi Target/Assets/02.Scripts/ARTrackedMultiImageManager.cs
@@ -71,10 +71,15 @@ public class ARTrackedMultiImageManager : MonoBehaviour
         string name = trackedImage.referenceImage.name;
         trackedObject = spawnedObjects[name];
 
+        if (trackedImage.trackingState == TrackingState.None)
+        {
+            trackedObject.transform.position = trackedImage.transform.position;
+        }
+
         // 이미지의 추적 상태가 추적중(Tracking)일 때
         if (trackedImage.trackingState == TrackingState.Tracking)
         {
-            trackedObject.transform.position = Vector3.MoveTowards(trackedObject.transform.position, new Vector3(trackedImage.transform.position.x, trackedImage.transform.position.y + 1f, trackedImage.transform.position.z), Time.deltaTime * 0.25f);
+            trackedObject.transform.position = Vector3.MoveTowards(trackedObject.transform.position, new Vector3(trackedImage.transform.position.x, trackedImage.transform.position.y + 1.0f, trackedImage.transform.position.z), Time.deltaTime);
             trackedObject.transform.rotation = trackedImage.transform.rotation;
             trackedObject.SetActive(true);
         }
