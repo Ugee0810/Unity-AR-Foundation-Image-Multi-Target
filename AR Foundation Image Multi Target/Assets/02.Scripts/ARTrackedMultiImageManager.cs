875f065894402c745dd99e83ad2208ff44be41ae
Ugee99 <shu990810@naver.com>
2022-12-01 09:43:34 +0900

    .

--- AR Foundation Image Multi Target/Assets/02.Scripts/ARTrackedMultiImageManager.cs
+++ AR Foundation Image Multi Target/Assets/02.Scripts/ARTrackedMultiImageManager.cs
@@ -14,8 +14,12 @@ public class ARTrackedMultiImageManager : MonoBehaviour
     Dictionary<string, GameObject> spawnedObjects = new Dictionary<string, GameObject>();
     ARTrackedImageManager trackedImageManager;
 
+    public Text text;
+    public Transform target;
+
     private void Awake()
     {
+        text.GetComponent<GameObject>();
         // AR Session Origin 오브젝트에 컴포넌트로 적용했을 때 사용 가능
         trackedImageManager = GetComponent<ARTrackedImageManager>();
         // trackedPrefabs 배열에 있는 모든 프리팹을 Instantiate()로 생성한 후
@@ -71,21 +75,28 @@ public class ARTrackedMultiImageManager : MonoBehaviour
         string name = trackedImage.referenceImage.name;
         trackedObject = spawnedObjects[name];
 
-        if (trackedImage.trackingState == TrackingState.None)
-        {
-            trackedObject.transform.position = trackedImage.transform.position;
-        }
+        //if (trackedImage.trackingState == TrackingState.None)
+        //{
+        //    trackedObject.transform.position = trackedImage.transform.position;
+        //}
 
         // 이미지의 추적 상태가 추적중(Tracking)일 때
         if (trackedImage.trackingState == TrackingState.Tracking)
         {
-            trackedObject.transform.position = Vector3.MoveTowards(trackedObject.transform.position, new Vector3(trackedImage.transform.position.x, trackedImage.transform.position.y + 1.0f, trackedImage.transform.position.z), Time.deltaTime);
-            trackedObject.transform.rotation = trackedImage.transform.rotation;
+            text.enabled = true;
+
+            //trackedObject.transform.position = Vector3.MoveTowards(trackedObject.transform.position, new Vector3(trackedImage.transform.position.x, trackedImage.transform.position.y + 1.0f, trackedImage.transform.position.z), Time.deltaTime);
+            trackedObject.transform.position = trackedImage.transform.position;
+            //trackedObject.transform.rotation = trackedImage.transform.rotation;
+            //Vector3 relativePos = target.position - trackedObject.transform.position;
+            //Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+            //trackedObject.transform.rotation = rotation;
+
             trackedObject.SetActive(true);
         }
         else
         {
-            trackedObject.transform.position = trackedImage.transform.position;
+            text.enabled = false;
             trackedObject.SetActive(false);
         }
     }
